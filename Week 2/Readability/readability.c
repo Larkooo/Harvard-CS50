ae52aeabdc20af368221a8f125a3b80ba932aa31
Larko <59736843+Larkooo@users.noreply.github.com>
2021-03-14 14:29:27 -0400

    fixed indent

--- Week 2/Readability/readability.c
+++ Week 2/Readability/readability.c
@@ -37,9 +37,9 @@ int main(void)
         }
     }
 
-    #ifdef DEBUG // only for debug
+#ifdef DEBUG // only for debug
     printf("%i letter(s)\n%i word(s)\n%i sentence(s)\n", lettersCount, wordsCount, sentencesCount);
-    #endif
+#endif
 
     int grade = calculateGrade(lettersCount, wordsCount, sentencesCount);
     if (grade > 16) 
