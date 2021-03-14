d13b68efd7e4de0d6da6b070af2f6e8462e7907a
Larko <59736843+Larkooo@users.noreply.github.com>
2021-03-14 13:31:23 -0400

    added comments

--- Week 1/Mario/mario.c
+++ Week 1/Mario/mario.c
@@ -31,21 +31,28 @@ int getHeight(void)
 
 void drawBricks(int height)
 {
+    // Iterate over columns, height
     for (int i = 1; i <= height; i++) 
     {
+        // 3 iterations for the rows, this one is for the spaces before
+        // drawing the bricks
         for (int n = 0; n < height - i; n++) 
         {
             printf(" ");
         }
+        // drawing first line of bricks
         for (int n = 0; n < i; n++) 
         {
             printf("#");
         }
+        // space between the second line of bricks
         printf("  ");
+        // drawing the second line of bricks
         for (int n = 0; n < i; n++) 
         {
             printf("#");
         }
+        // jump to the next line
         printf("\n");
     }
 }
